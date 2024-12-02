22852448ff3a673d1d310b0f541b32967fb23f4c
David Leek <david@getunleash.io>
2024-12-02 14:27:35 +0100

    chore: init

---
+++ init/dbsetup.sql
@@ -0,0 +1,5 @@
+CREATE USER unleash_user WITH PASSWORD 'password';
+CREATE DATABASE unleash WITH OWNER unleash_user;
+
+CREATE USER releash_user WITH PASSWORD 'repassword';
+CREATE DATABASE releash WITH OWNER releash_user;
